86132bfb57bb91a9b54f57002868439798bcbfe5
sophiekumagai <sophie.satie20@gmail.com>
2024-09-17 10:02:17 -0300

    asdf

--- src/components/Itens/Itens.jsx
+++ src/components/Itens/Itens.jsx
@@ -2,7 +2,7 @@ import styles from "../Itens/Itens.module.css"
 function Itens({lista_produtos}) {
     return (
         <div className={styles.container}>
-            <ul className={styles.produtos_list}>
+            <ul className={styles.container}>
                 {lista_produtos.map((item) => ( 
                     item
                 ))}
